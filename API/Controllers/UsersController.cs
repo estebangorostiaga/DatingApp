d16661f6733e3fef4c5fed5c5495bb681bfaf25c
Esteban <egorosti@purdue.edu>
2023-09-01 17:16:54 -0400

    Section 4 Commit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,21 +1,22 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // /api/users
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly DataContext  _context;
 
         public UsersController(DataContext context)
         {
             _context = context;  // Way to assign private fields to constructor
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -23,6 +24,7 @@ namespace API.Controllers
 
             return users;
         }
+
 
         [HttpGet("{id}")] // api/user/2
         public async Task<ActionResult<AppUser>> GetUser( int id)
