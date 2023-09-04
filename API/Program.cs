8e28cb80c891947193ee4f532f871d6567f0914d
Esteban <egorosti@purdue.edu>
2023-09-04 14:30:14 -0400

    Section 7 Commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // Removed Swagger and it's services not used
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("https://localhost:4200"));
