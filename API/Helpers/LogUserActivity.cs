16ddd7b3a701707743a5039db58628318dbedf4b
Esteban <egorosti@purdue.edu>
2023-09-08 00:29:08 -0400

    Section 13

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,24 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next(); // Gives us the action executed context
+
+            if(!resultContext.HttpContext.User.Identity.IsAuthenticated) return; // Not necessary since our User is already authenticated
+
+            var userId = resultContext.HttpContext.User.GetUserId();
+
+            var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+            var user = await repo.GetUserByIdAsync(int.Parse(userId));
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
+    }
+}
+
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -6,6 +6,11 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TimeagoModule } from "ngx-timeago";
+
+
 
 
 @NgModule({
@@ -21,7 +26,10 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
       type: 'ball-8bits'
     }),
     FileUploadModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -30,6 +38,9 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     NgxSpinnerModule,
     FileUploadModule,
     BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+    TimeagoModule,
   ]
 })
 export class SharedModule { }
